86f090c853f8d43cc1040c007260287b19bed772
Muzaffar-prog <murodovmuzaffar013@gmail.com>
2021-05-19 15:50:26 +0500

    Yangi dars qushildi
    For skili buyicha yangi dars yuklandi

---
+++ for_sikil_ishlash.py
@@ -0,0 +1,32 @@
+
+# For tsikil bilan ishlashni urganamiz!!!
+
+# mexmonlar = ['alijon', 'fazliddin', 'jasur','nurullo','shokir']
+# # for mexmon in mexmonlar:
+# #     print("Salom,",mexmon.capitalize())
+#
+# for i in mexmonlar:
+#     print(f"Assalomu alaykum hurmatli {i.capitalize()}, sizni 2022-yil 5-noyabr kuni bo'lib utadigan tuyimizga"
+#           f"taklif etamiz, Yuldoshovlar xonadoni\n")
+#
+# sonlar = list(range(1,6))
+# for son in sonlar:
+#     print(f"{son},ning kvadrati {son**2} ga teng ekan!")
+
+
+# print('\n')
+# dostlar = []
+# print("Eng yaqin 5-ta do'stingizning ismimi kiritng: ")
+# for i in range(5):
+#     dostlar.append(input(f"{i+1}-chi do'stingizni ismimi kirintg: "))
+# print("Sizning do'stlaringiz: ",dostlar)
+
+ #Amaliyot
+kinolar = []
+print("Eng yoqtirgan 5 ta kino nomini yozing: ")
+for kino in range(5):
+    kinolar.append(input(f"{kino+1},chi kino nomini kiritng: "))
+print(kinolar)
+print("Assalomu alaykum")
+
+for i in range()
